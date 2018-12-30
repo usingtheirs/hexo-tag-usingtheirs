fd1c36f5db8218e772ef83ea40ffe6c17758cae6
usingtheirs <43819005+usingtheirs@users.noreply.github.com>
2018-12-30 22:35:39 +0900

    ut_img has no ending tag

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 var ctx = hexo;
 var tag = ctx.extend.tag;
 
-tag.register('ut_img', require('./ut_img')(ctx), true);
+tag.register('ut_img', require('./ut_img')(ctx));
 tag.register('ut_aside', require('./ut_aside')(ctx), true);
