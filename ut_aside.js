760575197dcdb47c345cc02377ae8958c442fa93
usingtheirs <43819005+usingtheirs@users.noreply.github.com>
2018-12-30 22:38:13 +0900

    args[0]

--- ut_aside.js
+++ ut_aside.js
@@ -10,6 +10,6 @@
 module.exports = function(ctx) {
 	return function ut_aside_tag(args, content) {
 		content = ctx.render.renderSync({text: content, engine: 'markdown'});
-    return '<aside class="ut_aside"><h3>args[0]</h3><div class="collapsed">' + content +'</div></aside>';
+    		return '<aside class="ut_aside"><h3>' + args[0] + '</h3><div class="collapsed">' + content +'</div></aside>';
 	};
 };
