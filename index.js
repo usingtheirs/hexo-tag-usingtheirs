6c9b4a139782b868e7e0c152e32f70dba1d971cc
usingtheirs <43819005+usingtheirs@users.noreply.github.com>
2018-12-31 12:20:07 +0900

    Added ut_gfycat

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ var tag = ctx.extend.tag;
 
 tag.register('ut_img', require('./ut_img')(ctx));
 tag.register('ut_aside', require('./ut_aside')(ctx), true);
+tag.register('ut_gfycat', require('./ut_gfycat')(ctx));
+
