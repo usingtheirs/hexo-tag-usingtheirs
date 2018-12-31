2509bc1a9671462468415543debe4e47ed10a0c3
usingtheirs <43819005+usingtheirs@users.noreply.github.com>
2018-12-31 13:18:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ var tag = ctx.extend.tag;
 tag.register('ut_img', require('./ut_img')(ctx));
 tag.register('ut_aside', require('./ut_aside')(ctx), true);
 tag.register('ut_gfycat', require('./ut_gfycat')(ctx));
-
+tag.register('ut_figure', require('./ut_figure')(ctx));
