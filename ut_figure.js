c23a0e4ba4d2b7361bdd5597a408cf9567d55ae2
usingtheirs <43819005+usingtheirs@users.noreply.github.com>
2018-12-31 13:19:19 +0900

    Update ut_figure.js

--- ut_figure.js
+++ ut_figure.js
@@ -10,7 +10,7 @@ module.exports = ctx => {
   return function ut_figureTag(args, content) {
     const len = args.length;
 
-    const caption = args.join(' '));
+    const caption = args.join(' ');
     content = ctx.render.renderSync({text: content, engine: 'markdown'});
 
     return `<figure>${content}<figcaption>${caption}</figcaption></figure>`;
