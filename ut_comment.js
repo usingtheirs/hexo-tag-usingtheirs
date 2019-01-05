f17b6feff437521a179f4aed5e5dedd58838d491
usingtheirs <43819005+usingtheirs@users.noreply.github.com>
2019-01-05 23:06:02 +0900

    Create ut_comment.js

---
+++ ut_comment.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+* Comment Tag
+*
+* Syntax:
+*   {% ut_comment%}Comments Here{%endut_comment%}
+*/
+module.exports = ctx => {
+  return function ut_commentTag(args, content) {
+    return '';
+  }
+};
