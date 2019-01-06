45ac5ef1c9631c73dce1059a4c4f5b03f9fa9d71
usingtheirs <43819005+usingtheirs@users.noreply.github.com>
2019-01-06 21:16:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ tag.register('ut_img', require('./ut_img')(ctx));
 tag.register('ut_aside', require('./ut_aside')(ctx), true);
 tag.register('ut_gfycat', require('./ut_gfycat')(ctx));
 const ut_figure = require('./ut_figure');
-tag.register('ut_figure_open', ut_figure.open(ctx), true);
-tag.register('ut_figure_close', ut_figure.close(ctx), true);
+tag.register('ut_figure_open', ut_figure.open(ctx));
+tag.register('ut_figure_close', ut_figure.close(ctx));
 tag.register('ut_comment', require('./ut_comment')(ctx), true);
